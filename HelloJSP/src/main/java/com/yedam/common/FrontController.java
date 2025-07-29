0102e6083ca279e7970c409ddf6e94dcb7e5d01b
chichi <cyannara@naver.com>
2025-07-30 08:59:58 +0900

    페이징.

--- HelloJSP/src/main/java/com/yedam/common/FrontController.java
+++ HelloJSP/src/main/java/com/yedam/common/FrontController.java
@@ -10,6 +10,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.yedam.control.AddBoardControl;
+import com.yedam.control.BoardControl;
+import com.yedam.control.BoardListControl;
+import com.yedam.control.RegisterControl;
+
 // init - service - detroy
 // *.do -> 실행할 컨트롤.
 // 요청url === 실행할 컨트롤.
@@ -25,11 +30,22 @@ public class FrontController extends HttpServlet {
 
 	@Override
 	public void init(ServletConfig config) throws ServletException {
-		map.put("boardList.do", null);
+		map.put("/boardList.do", new BoardListControl());
+		map.put("/board.do", new BoardControl());
+		map.put("/register.do", new RegisterControl()); // 등록화면.
+		map.put("/addBoard.do", new AddBoardControl()); // 등록처리.
 	}
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// url vs. uri
+		// http://localhost:8080/HelloJSP/boardList.do => url
+		// /HelloJSP/boardList.do                      => uri
+		String uri = req.getRequestURI();
+		String context = req.getContextPath(); // /HelloJSP
+		String page = uri.substring(context.length()); // /boardList.do
 
+		Control control = map.get(page);
+		control.execute(req, resp);
 	}
 }
