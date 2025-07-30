de555e7d07f4ecf0a50f710f0ebb172ee979fa47
chichi <cyannara@naver.com>
2025-07-30 17:51:02 +0900

    페이징, jstl 까지.

---
+++ HelloJSP/src/main/java/com/yedam/common/SearchDTO.java
@@ -0,0 +1,11 @@
+package com.yedam.common;
+
+import lombok.Data;
+
+// 글목록에 사용되는 파라미터르 담는 클래스.
+@Data
+public class SearchDTO {
+	private int page;
+	private String searchCondition;
+	private String keyword;
+}
